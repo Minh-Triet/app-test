666e0b1dff2afc270e1f98d0066159ebd5c81244
null <null>
2023-05-19 09:29:30 +0000

    â

--- development_config.py
+++ development_config.py
@@ -41,7 +41,7 @@ job_defaults = {
 scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors,
                                 job_defaults=job_defaults, timezone='Asia/ho_chi_minh')
 
-redis = RedisCluster(url='redis://localhost:6379', decode_responses=True)
+redis = Redis(url='redis://localhost:6379', decode_responses=True)
 
 
 class Config:
