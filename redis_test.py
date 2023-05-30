af763ce0a47973007c97eb1cda068cc193e837ff
Admins <bmt2000gg@gmail.com>
2023-05-30 14:10:24 +0700

    update config

---
+++ redis_test.py
@@ -0,0 +1,10 @@
+from redis import Redis
+conn = Redis()
+
+import redis_lock
+lock = redis_lock.Lock(conn, "name-of-the-lock")
+if lock.acquire(blocking=False):
+    print("Got the lock.")
+    lock.release()
+else:
+    print("Someone else has the lock.")
